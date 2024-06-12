027ab276d9d91eef77bb68184b12093a31bc1565
srujankumar07 <balgonisrujan2002@gmail.com>
2024-06-12 17:49:50 +0530

    lol

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
 	cors({
-	  origin: [
-		"http://localhost:3000",
-		"https://bloodbankfront.vercel.app"
-	  ],
+	  origin: ["http://localhost:3000","https://bloodbankfront.vercel.app"],
 	  credentials: true,
 	})
   );
