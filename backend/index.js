ed28f49c619e3c23520e50f0a01c69c7d9491286
srujankumar07 <balgonisrujan2002@gmail.com>
2024-10-04 11:26:12 +0530

    v5

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(
 	cors({
 	  origin: ["http://localhost:3000","https://bloodbankfront.vercel.app"],
+	  methods: 'GET,POST,PUT,DELETE',
 	  credentials: true,
 	})
   );
